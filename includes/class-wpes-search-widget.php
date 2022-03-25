b24a24e0d22b33514a7be5694f711b385da7cddf
Alex Lion <learnwithalex@gmail.com>
2022-03-25 11:58:59 -0700

    Add missing text-domain (#8)
    * Add missing text-domain
    
    Add these missing text-domain let related strings display correctly.
    
    * Add more strings to be localizable

--- includes/class-wpes-search-widget.php
+++ includes/class-wpes-search-widget.php
@@ -144,7 +144,7 @@ class WPES_Search_Widget extends WP_Widget {
 		$all_settings = WPES()->wpes_admin->get_all_setting_names(); ?>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'wp-extended-search' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
 		</p>
 		<?php
@@ -162,7 +162,7 @@ class WPES_Search_Widget extends WP_Widget {
 		}
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'wpessid' ); ?>"><?php _e( 'Setting Name:' ); ?></label>
+			<label for="<?php echo $this->get_field_id( 'wpessid' ); ?>"><?php _e( 'Setting Name:', 'wp-extended-search' ); ?></label>
 			<select class="widefat" id="<?php echo $this->get_field_id( 'wpessid' ); ?>" name="<?php echo $this->get_field_name( 'wpessid' ); ?>">
 				<option value=""><?php _e( 'Global (default)', 'wp-extended-search' ); ?></option>
 				<?php
